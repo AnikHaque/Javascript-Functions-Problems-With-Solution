927c64d30ca7da462d2324ac7168a854e9a8f179
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 02:02:50 +0600

    Update problem02.js

--- problem02.js
+++ problem02.js
@@ -2,3 +2,13 @@
 // Examples:
 // giveMeSomething("is better than nothing") ➞ "something is better than nothing"
 // giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+// solution : 
+function getLastItem(arr) {
+  if (Array.isArray(arr) && arr.length > 0) {
+    return arr[arr.length - 1];
+  } else {
+    // Return some default value or handle the case when the array is empty or not an array
+    return undefined;
+  }
+}
