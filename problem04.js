a85f8fcb922ccb0d4d9efb1ab4b662d81b50ed6b
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 02:00:35 +0600

    Update problem04.js

--- problem04.js
+++ problem04.js
@@ -1,1 +1,5 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
+// Examples:
+// lessThanOrEqualToZero(5) ➞ false
+// lessThanOrEqualToZero(0) ➞ true
