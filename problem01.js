0128b3d4f88c7040151c3e289b86f244aa962652
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 01:57:01 +0600

    Create problem01.js

---
+++ problem01.js
@@ -0,0 +1,14 @@
+// 1) Write a function that takes an integer minutes and converts it to seconds.
+// Examples:
+// convert(5) ➞ 300
+// convert(3) ➞ 180
+
+// Solution: 
+function getFirstValue(arr) {
+  if (Array.isArray(arr) && arr.length > 0) {
+    return arr[0];
+  } else {
+    // Return some default value or handle the case when the array is empty or not an array
+    return undefined;
+  }
+}
