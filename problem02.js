7b425903577e1dfa0caa3277d4419051fb3ee2a3
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 01:59:29 +0600

    Update problem02.js

--- problem02.js
+++ problem02.js
@@ -1,1 +1,4 @@
-
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+// Examples:
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+// giveMeSomething("Bob Jane") ➞ "something Bob Jane"
