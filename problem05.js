308fb9c83398100530e940127afd8c8a4e0300cf
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 02:01:01 +0600

    Update problem05.js

--- problem05.js
+++ problem05.js
@@ -1,1 +1,5 @@
+// Create a function that takes an array of numbers and returns the smallest number in the set.
 
+// Examples:
+// findSmallestNum([34, 15, 88, 2]) ➞ 2
+// findSmallestNum([34, -345, -1, 100]) ➞ -345
