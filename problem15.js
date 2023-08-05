0f79301a3858666f453ec9b11943a6cfe045e173
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 14:56:37 +0600

    Update problem15.js

--- problem15.js
+++ problem15.js
@@ -1,1 +1,8 @@
+//Create a function that will convert from Celsius to Fahrenheit
 
+function celsiusToFahrenheit(n) {
+    return n * 1.8 + 32;
+}
+
+var r = celsiusToFahrenheit(20);
+console.log(r);
